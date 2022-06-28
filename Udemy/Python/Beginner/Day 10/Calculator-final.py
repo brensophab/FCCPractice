748fb48c3610deaa02de2daf77a8021bfbbd0901
Sophabmixay <bresopha@amazon.com>
2022-06-27 18:15:46 -0700

    Day 10 Cont'd

--- Udemy/Python/Beginner/Day 10/Calculator-final.py
+++ Udemy/Python/Beginner/Day 10/Calculator-final.py
@@ -1,10 +1,11 @@
+from audioop import mul
+from operator import indexOf
 from art import logo
 print(logo)
 def clear():
     
     print('\n' * 100)
     print('\033[2J\033[1;1H', end='')
-
 #Calculator
 #add
 def add(n1, n2):
@@ -30,5 +31,23 @@ operations ={
 
 num1 = int(input("Whats the first number?:\n"))
 num2 = int(input("Whats the second number?\n"))
+
 for operation in operations:
-    print(operations) #INCOMPLETE: COMPLETE FOR LOOP TO PRINT OUT EVERY OPERATION IN THE OPERATIONS DICT
+    print(operation) #INCOMPLETE: COMPLETE FOR LOOP TO PRINT OUT EVERY OPERATION IN THE OPERATIONS DICT
+operation_symbol = input("Pick an operation from the lines above")
+# def calculate():
+#     if operation_symbol == "+":
+#         answer = add(num1, num2)
+#     elif operation_symbol == "-":
+#         answer = subtract(num1,num2)
+#     elif operation_symbol == "*":
+#         answer = multiply(num1,num2)
+#     elif operation_symbol == "/": 
+#         answer = divide(num1,num2)
+#     return answer
+# answer = calculate()
+
+#MY SOLUTION ^^ WORKED FINE, HOWEVER, THIS IS THE INTENDED SOLUTION WITH DICTS
+calc_func = operations[operation_symbol]
+answer = calc_func(num1, num2)
+print(f"{num1}{operation_symbol}{num2} = {answer}")
